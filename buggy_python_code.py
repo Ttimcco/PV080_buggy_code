711028fea074e83d98ce5d2f14ab1c990dc52507
Timotej Rumanovsky <xrumanov@nymfe126.fi.muni.cz>
2026-05-04 17:14:44 +0200

    codeql

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -21,14 +21,14 @@ def print_nametag(format_string, person):
     print(format_string.format(person=person))
 
 
+import urllib3
+
 def fetch_website(urllib_version, url):
-    # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
-    # Fetch and print the requested URL
- 
-    try: 
-        http = urllib.PoolManager()
-    except:
+    try:
+        http = urllib3.PoolManager()
+        r = http.request('GET', url)
+        print(r.data)
+    except Exception:
         print('Exception')
 
 
